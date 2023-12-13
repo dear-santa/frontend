17a8114c64e1992d0aa90dc925a84c3bcea030e4
sooyoungh <sooyoung.h8@gmail.com>
2023-12-13 16:22:33 +0900

    fix: api pat 수정

--- src/pages/LoginModal/KakaoCallback.jsx
+++ src/pages/LoginModal/KakaoCallback.jsx
@@ -5,7 +5,7 @@ function KakaoCallback() {
   //최초 렌더링 시 발동
   useEffect(() => {
     const code = new URL(window.location.href).searchParams.get("code");
-    console.log(code);
+    console.log("카카오에서 받은 인가 코드 : " + code);
     const sendData = JSON.stringify({
       token: code,
       oauthType: "KAKAO",
@@ -14,11 +14,11 @@ function KakaoCallback() {
     //spring 서버로 인증키를 통해 유저정보를 획득하고 로그인 처리 요청
     axios({
       method: "POST",
-      url: "/api/v1/auth/kakao/login",
+      url: "/api/v1/kakao/login",
       data: sendData,
       headers: {
         "Content-type": "application/json",
-        Authorization: "Bearer " + auth,
+        "Authorization": auth,
       },
     })
       .then((response) => {
