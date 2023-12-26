dc851a4d7febf1c8abc1f25e63677c159cd18211
sooyoungh <sooyoung.h8@gmail.com>
2023-12-27 02:20:30 +0900

    fix: 카카오 리다이렉트 uri 수정

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/board/:boardId" element={<BoardModal />} />
-        <Route path="/login" element={<KakaoCallback />} />
+        <Route path="/oauth" element={<KakaoCallback />} />
       </Routes>
     </Router>
   );
